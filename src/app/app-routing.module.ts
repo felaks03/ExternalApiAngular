1c2d98a8e056c957f81da79247b23f55887984af
felaks03 <felixruizdelacuesta1@gmail.com>
2022-08-16 18:40:09 +0200

    all components generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { E404Component } from './components/e404/e404.component';
+import { FormComponent } from './components/form/form.component';
+import { HomeComponent } from './components/home/home.component';
+import { UpdateFormComponent } from './components/update-form/update-form.component';
+import { ViewUserComponent } from './components/view-user/view-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'user/:id', component: ViewUserComponent},
+  {path: 'newuser', component: FormComponent},
+  {path: 'updateuser/:id', component: UpdateFormComponent},
+  {path: '**', component: E404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CardUserComponent } from './components/card-user/card-user.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewUserComponent } from './components/view-user/view-user.component';
+import { FormComponent } from './components/form/form.component';
+import { UpdateFormComponent } from './components/update-form/update-form.component';
+import { E404Component } from './components/e404/e404.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CardUserComponent,
+    HomeComponent,
+    ViewUserComponent,
+    FormComponent,
+    UpdateFormComponent,
+    E404Component,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
